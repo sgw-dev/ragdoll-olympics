3dc6d33963a269dd898c2952882251aea69c569f
Joseph Manley <j@cloudsumu.com>
2020-09-26 18:30:29 -0400

    Basic round and map loading system

---
+++ ragdoll-olympics/Assets/Scripts/DataObjects/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Data
+{
+    [System.Serializable]
+    public class Player
+    {
+        public string playerName;
+        public int goldMedals;
+        public int silverMedals;
+        public int bronzeMedals;
+        public int participationMedals;
+        public bool computer = false;
+    }
+}
---
+++ ragdoll-olympics/Assets/Scripts/ScriptableObjects/OlympicEvent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "event", menuName = "ScriptableObjects/Olympic Event", order = 1)]
+public class OlympicEvent : ScriptableObject
+{
+    public string eventName; // Human friendly name
+    public GameObject playerPrefab; // Player prefab spawned at starting location
+    public string eventUiScene;
+    public List<string> eventMaps; // Names of scenes containing maps
+}
